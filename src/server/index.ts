f98dc53167270a971303bdb572f8df4d07c3ea80
Andrew Ellis <andrew.ellis@mheducation.com>
2017-10-25 12:49:09 -0400

    add scripts and lock down

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,11 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 import routes from './routes';
 
 const app = express();
 
-app.use([bodyParser.json(),helmet()])
+//tsign
+app.use([express.json(),helmet()])
 
 app.set('view engine', 'hbs');
 app.set('views', './src/server/views');
